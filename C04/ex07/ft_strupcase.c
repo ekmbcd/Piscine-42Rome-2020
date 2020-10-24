af0aeccaf2663736ab96c5b6f0b1ce3c3ce069d4
Andrea De Felice <ekmbcd@gmail.com>
2020-10-24 19:47:19 +0200

    1

---
+++ C04/ex07/ft_strupcase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char *ft_Strupcase(char *str)
+{
+	int index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+
+	char res[index + 2];
+	res[index + 1] = '\n';
+
+	while (index != 0)
+	{
+		}
+}
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
